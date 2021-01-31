c7288796aa198fbe5268a8f1755e50fc2cb6f923
Łukasz Łopata <lukasz.lopata96@gmail.com>
2021-01-31 17:37:55 +0100

    Part 2

--- SeleniumCSharpTutorials/BaseClass/BaseTest.cs
+++ SeleniumCSharpTutorials/BaseClass/BaseTest.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 
@@ -22,14 +23,20 @@ namespace SeleniumCSharpTutorials.BaseClass
             driver = new ChromeDriver(options);
             driver.Manage().Window.Maximize();
             driver.Url = "https://www.facebook.com/";
+
             IWebElement cookiesAcceptation = driver.FindElement(By.XPath(".//*[@id='u_0_h']"));
+            IWebElement registerButton = driver.FindElement(By.XPath(".//*[@id='u_0_2']"));
+            
             cookiesAcceptation.Click();
+            Thread.Sleep(1000);
+            registerButton.Click();
+            Thread.Sleep(1000);
         }
 
         [OneTimeTearDown]
         public void Close()
         {
-            driver.Quit();
+            //driver.Quit();
         }
 
     }
--- SeleniumCSharpTutorials/TestClass.cs
+++ SeleniumCSharpTutorials/TestClass.cs
@@ -7,6 +7,7 @@ using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using SeleniumCSharpTutorials.BaseClass;
 
 namespace SeleniumCSharpTutorials
@@ -17,8 +18,14 @@ namespace SeleniumCSharpTutorials
         [Test]
         public void TestMethod1()
         {
-            IWebElement emailTextField =  driver.FindElement(By.XPath(".//*[@id='email']"));
-            emailTextField.SendKeys("Selenium C#");
+            IWebElement nameTextField =  driver.FindElement(By.XPath(".//*[@id='u_3_b']"));
+            nameTextField.SendKeys("Selenium C#");
+
+            IWebElement monthDropdownList = driver.FindElement(By.XPath(".//*[@id='month']"));
+            SelectElement element = new SelectElement(monthDropdownList);
+            element.SelectByIndex(1);
+            element.SelectByText("mar");
+            element.SelectByValue("6");
         }
 
         [Test]
