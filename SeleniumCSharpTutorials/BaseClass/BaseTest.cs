259c95d2de3aa6aeb09e15fa55173c05219d7d54
Łukasz Łopata <lukasz.lopata96@gmail.com>
2021-01-31 15:06:21 +0100

    Add project files.

---
+++ SeleniumCSharpTutorials/BaseClass/BaseTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SeleniumCSharpTutorials.BaseClass
+{
+    
+    public class BaseTest
+    {
+        public IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void Open()
+        {
+            ChromeOptions options = new ChromeOptions() { BinaryLocation = @"C:\Program Files\Google\Chrome\Application\chrome.exe" };
+            driver = new ChromeDriver(options);
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.facebook.com/";
+            IWebElement cookiesAcceptation = driver.FindElement(By.XPath(".//*[@id='u_0_h']"));
+            cookiesAcceptation.Click();
+        }
+
+        [OneTimeTearDown]
+        public void Close()
+        {
+            driver.Quit();
+        }
+
+    }
+}
